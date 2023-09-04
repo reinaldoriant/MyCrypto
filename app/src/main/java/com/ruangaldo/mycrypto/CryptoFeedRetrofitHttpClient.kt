7f3a1533772b0944d6f89e3fca09d77ae4806c28
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 16:36:25 +0700

    + add invalid data response with httpexception

--- app/src/main/java/com/ruangaldo/mycrypto/CryptoFeedRetrofitHttpClient.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/CryptoFeedRetrofitHttpClient.kt
@@ -1,11 +1,13 @@
 package com.ruangaldo.mycrypto
 
 import com.ruangaldo.mycrypto.domain.Connectivity
+import com.ruangaldo.mycrypto.domain.InvalidData
 import java.io.IOException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import retrofit2.HttpException
 
 /**
  * Written with joy and smile by Ruang Aldo on 04/09/23.
@@ -17,6 +19,12 @@ class CryptoFeedRetrofitHttpClient: HttpClient {
         try { } catch (throwable: Throwable) {
             if (throwable is IOException) {
                 emit(HttpClientResult.Failure(Connectivity()))
+            }
+
+            if (throwable is HttpException) {
+                if (throwable.code() == 422) {
+                    HttpClientResult.Failure(InvalidData())
+                }
             }
         }
     }.flowOn(Dispatchers.IO)
