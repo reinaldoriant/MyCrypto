f8dd86fa67c1fdeabe447608c51b17405ddbf8ae
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 16:56:19 +0700

    + add CryptoFeedItemsMapper

---
+++ app/src/main/java/com/ruangaldo/mycrypto/domain/CryptoFeedItemsMapper.kt
@@ -0,0 +1,31 @@
+package com.ruangaldo.mycrypto.domain
+
+import com.ruangaldo.mycrypto.http.RemoteCryptoFeedItem
+
+/**
+ * Written with joy and smile by Ruang Aldo on 04/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+class CryptoFeedItemsMapper {
+    companion object {
+        fun map(items: List<RemoteCryptoFeedItem>): List<CryptoFeedItem> {
+            return items.map {
+                CryptoFeedItem(
+                    coinInfo = CoinInfoItem(
+                        it.remoteCoinInfo.id.orEmpty(),
+                        it.remoteCoinInfo.name.orEmpty(),
+                        it.remoteCoinInfo.fullName.orEmpty(),
+                        it.remoteCoinInfo.imageUrl.orEmpty()
+                    ),
+                    raw = RawItem(
+                        usd = UsdItem(
+                            it.remoteRaw.usd.price ?: 0.0,
+                            it.remoteRaw.usd.changePctDay ?: 0F
+                        )
+                    )
+                )
+            }
+        }
+    }
+}
