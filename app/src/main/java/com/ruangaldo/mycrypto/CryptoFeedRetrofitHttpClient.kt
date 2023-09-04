7d48fd9f37e165ca527af978ad80b664f773bf83
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 16:22:54 +0700

    ! add and move CryptoFeedRetrofitHttpClient

---
+++ app/src/main/java/com/ruangaldo/mycrypto/CryptoFeedRetrofitHttpClient.kt
@@ -0,0 +1,24 @@
+package com.ruangaldo.mycrypto
+
+import com.ruangaldo.mycrypto.domain.Connectivity
+import com.ruangaldo.mycrypto.domain.HttpClientResult
+import java.io.IOException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+/**
+ * Written with joy and smile by Ruang Aldo on 04/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+class CryptoFeedRetrofitHttpClient {
+    fun get(): Flow<HttpClientResult> = flow {
+        try { } catch (throwable: Throwable) {
+            if (throwable is IOException) {
+                emit(HttpClientResult.Failure(Connectivity()))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+}
--- app/src/main/java/com/ruangaldo/mycrypto/domain/RemoteCryptoFeedLoader.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/domain/RemoteCryptoFeedLoader.kt
@@ -1,10 +1,8 @@
 package com.ruangaldo.mycrypto.domain
 
-import java.io.IOException
-import kotlinx.coroutines.Dispatchers
+import com.ruangaldo.mycrypto.CryptoFeedRetrofitHttpClient
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 
 /**
  * Written with joy and smile by Ruang Aldo on 04/09/23.
@@ -13,16 +11,6 @@ import kotlinx.coroutines.flow.flowOn
 
 sealed class HttpClientResult {
     data class Failure(val throwable: Throwable) : HttpClientResult()
-}
-
-class CryptoFeedRetrofitHttpClient {
-    fun get(): Flow<HttpClientResult> = flow {
-        try { } catch (throwable: Throwable) {
-            if (throwable is IOException) {
-                emit(HttpClientResult.Failure(Connectivity()))
-            }
-        }
-    }.flowOn(Dispatchers.IO)
 }
 
 class RemoteCryptoFeedLoader constructor(private val httpClient: CryptoFeedRetrofitHttpClient) {
