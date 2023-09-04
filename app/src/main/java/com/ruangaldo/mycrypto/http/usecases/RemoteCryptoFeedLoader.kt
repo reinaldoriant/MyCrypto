caab37921850a9391069802bfba6c33d7f6967b6
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-05 06:33:55 +0700

    ! modify domain error

--- app/src/main/java/com/ruangaldo/mycrypto/http/usecases/RemoteCryptoFeedLoader.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/http/usecases/RemoteCryptoFeedLoader.kt
@@ -1,5 +1,11 @@
 package com.ruangaldo.mycrypto.http.usecases
 
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import com.ruangaldo.mycrypto.domain.CryptoFeedItemsMapper
 import com.ruangaldo.mycrypto.domain.CryptoFeedLoader
 import com.ruangaldo.mycrypto.domain.CryptoFeedResult
@@ -7,6 +13,7 @@ import com.ruangaldo.mycrypto.http.ConnectivityException
 import com.ruangaldo.mycrypto.http.CryptoFeedHttpClient
 import com.ruangaldo.mycrypto.http.HttpClientResult
 import com.ruangaldo.mycrypto.http.InvalidDataException
+import com.ruangaldo.mycrypto.presentation.CryptoFeedUiState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -19,24 +26,29 @@ class RemoteCryptoFeedLoader constructor(private val cryptoFeedHttpClient: Crypt
     CryptoFeedLoader {
     override fun load(): Flow<CryptoFeedResult> = flow {
         cryptoFeedHttpClient.get().collect { result ->
-            if (result is HttpClientResult.Success) {
-                val cryptoFeed = result.root.data
-                if (cryptoFeed.isNotEmpty()) {
-                    emit(CryptoFeedResult.Success(CryptoFeedItemsMapper.map(cryptoFeed)))
-                } else {
-                    emit(CryptoFeedResult.Success(emptyList()))
-                }
-            }
-
-            if (result is HttpClientResult.Failure) {
-                if (result.throwable is InvalidDataException) {
-                    emit(CryptoFeedResult.Failure(InvalidData()))
+            when (result) {
+                is HttpClientResult.Success -> {
+                    val cryptoFeed = result.root.data
+                    if (cryptoFeed.isNotEmpty()) {
+                        emit(CryptoFeedResult.Success(CryptoFeedItemsMapper.map(cryptoFeed)))
+                    } else {
+                        emit(CryptoFeedResult.Success(emptyList()))
+                    }
                 }
 
-                if (result.throwable is ConnectivityException) {
-                    emit(CryptoFeedResult.Failure(Connectivity()))
+                is HttpClientResult.Failure -> {
+                    Log.d("loadCryptoFeed", "Failure")
+                    when (result.throwable) {
+                        is ConnectivityException -> {
+                            emit(CryptoFeedResult.Failure(Connectivity()))
+                        }
+
+                        is InvalidDataException -> {
+                            Log.d("loadCryptoFeed", "InvalidData")
+                            emit(CryptoFeedResult.Failure(InvalidData()))
+                        }
+                    }
                 }
-            }
         }
     }
 }
