31203139cad8a8ab80778f445c7c5b827eb0e9b6
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-05 06:32:25 +0700

    + add show loading indicator and pull refresh when view is presented

--- app/src/main/java/com/ruangaldo/mycrypto/MainActivity.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/MainActivity.kt
@@ -7,21 +7,28 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.PullRefreshState
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ruangaldo.mycrypto.presentation.CryptoFeedUiState
 import com.ruangaldo.mycrypto.presentation.CryptoFeedViewModel
 import com.ruangaldo.mycrypto.ui.theme.MyCryptoTheme
+import com.ruangaldo.mycrypto.ui.theme.Purple40
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,17 +58,58 @@ fun CryptoFeedRoute(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun CryptoFeedScreen(
     modifier: Modifier = Modifier,
     cryptoFeedUiState: CryptoFeedUiState,
 ) {
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = cryptoFeedUiState.isLoading,
+        onRefresh = {}
+    )
+
     Scaffold(topBar = {
-        CenterAlignedTopAppBar(title = {
-            Text("Crypto Feed", maxLines = 1)
-        })
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = "Crypto Feed",
+                    maxLines = 1,
+                    color = Color.White
+                )
+            },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = Purple40
+            )
+        )
     }, content = {
+        val contentModifier = modifier
+            .padding(it)
+            .fillMaxSize()
+
+        LoadingContent(
+            pullRefreshState = pullRefreshState,
+            loading = cryptoFeedUiState.isLoading,
+            empty = when (cryptoFeedUiState) {
+                is CryptoFeedUiState.HasCryptoFeed -> false
+                is CryptoFeedUiState.NoCryptoFeed -> cryptoFeedUiState.isLoading
+            },
+            emptyContent = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center)
+                ) {
+                    PullRefresh(
+                        loading = cryptoFeedUiState.isLoading,
+                        pullRefreshState = pullRefreshState,
+                        Modifier.align(Alignment.TopCenter)
+                    )
+                }
+            },
+            content = {
+            }
+        )
         Box(
             modifier = modifier
                 .padding(it)
@@ -73,4 +121,40 @@ fun CryptoFeedScreen(
             )
         }
     })
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun LoadingContent(
+    pullRefreshState: PullRefreshState,
+    loading: Boolean,
+    empty: Any,
+    emptyContent: () -> Unit,
+    content: () -> Unit
+) {
+    if (empty as Boolean) {
+        emptyContent()
+    } else {
+        Box(
+            modifier = Modifier, contentAlignment = Alignment.Center
+        ) {
+            content()
+
+            PullRefresh(
+                loading = loading,
+                pullRefreshState = pullRefreshState,
+                Modifier.align(Alignment.TopCenter)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PullRefresh(loading: Boolean, pullRefreshState: PullRefreshState, modifier: Modifier) {
+    PullRefreshIndicator(
+        refreshing = loading,
+        state = pullRefreshState,
+        modifier = modifier
+    )
 }
