f3e773f27043e5e2fc2b1b19b6b592be2ee4c733
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 16:16:04 +0700

    + add crypto feed loader

---
+++ app/src/main/java/com/ruangaldo/mycrypto/domain/CryptoFeedLoader.kt
@@ -0,0 +1,18 @@
+package com.ruangaldo.mycrypto.domain
+
+import kotlinx.coroutines.flow.Flow
+
+
+/**
+ * Written with joy and smile by Ruang Aldo on 04/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+sealed class CryptoFeedResult {
+    data class Success(val cryptoFeedItems: List<CryptoFeedItem>) : CryptoFeedResult()
+    data class Failure(val throwable: Throwable) : CryptoFeedResult()
+}
+
+interface CryptoFeedLoader {
+    fun load(): Flow<CryptoFeedResult>
+}
