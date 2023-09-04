0edb8190c23291333a519bf24a56599d728cdb85
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 17:06:02 +0700

    ! move CryptoFeedService to own file

--- app/src/main/java/com/ruangaldo/mycrypto/http/CryptoFeedRetrofitHttpClient.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/http/CryptoFeedRetrofitHttpClient.kt
@@ -8,22 +8,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import retrofit2.HttpException
-import retrofit2.http.GET
-import retrofit2.http.Query
 
 /**
  * Written with joy and smile by Ruang Aldo on 04/09/23.
  * Github: https://github.com/reinaldoriant
  */
-
-
-interface CryptoFeedService {
-    @GET("data/top/totaltoptiervolfull")
-    suspend fun get(
-        @Query("limit") limit: Int? = 20,
-        @Query("tsym") tsym: String? = "USD"
-    ): RemoteRootCryptoFeed
-}
 
 class CryptoFeedRetrofitHttpClient constructor(private val cryptoFeedService: CryptoFeedService) :
     HttpClient {
