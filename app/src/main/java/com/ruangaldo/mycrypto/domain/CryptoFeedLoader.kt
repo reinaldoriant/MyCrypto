2d783c7661256f15e2876a7494af1087e5dfa30d
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 19:05:58 +0700

    ! change name package with v26 caused error

--- app/src/main/java/com/ruangaldo/mycrypto/domain/CryptoFeedLoader.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/domain/CryptoFeedLoader.kt
@@ -1,6 +1,14 @@
 package com.ruangaldo.mycrypto.domain
 
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.ruangaldo.mycrypto.factories.RemoteCryptoFeedLoaderFactory
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 
 /**
@@ -15,4 +23,31 @@ sealed class CryptoFeedResult {
 
 interface CryptoFeedLoader {
     fun load(): Flow<CryptoFeedResult>
+}
+
+class CryptoFeedViewModel constructor(
+    private val cryptoFeedLoader: CryptoFeedLoader
+) : ViewModel() {
+
+    init {
+        loadCryptoFeed()
+    }
+
+    private fun loadCryptoFeed() {
+        viewModelScope.launch {
+            cryptoFeedLoader.load().collect { result ->
+                Log.d("CryptoFeedViewModel", "loadCryptoFeed execute $result")
+            }
+        }
+    }
+
+    companion object {
+        val FACTORY: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                CryptoFeedViewModel(
+                    RemoteCryptoFeedLoaderFactory.createRemoteCryptoFeedLoader()
+                )
+            }
+        }
+    }
 }
