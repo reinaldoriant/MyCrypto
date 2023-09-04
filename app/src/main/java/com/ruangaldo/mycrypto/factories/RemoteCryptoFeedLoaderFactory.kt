429d0b468d3ce575853f57b733a632a6c597f7e6
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 18:26:41 +0700

    + create RemoteCryptoFeedLoaderFactory

---
+++ app/src/main/java/com/ruangaldo/mycrypto/factories/RemoteCryptoFeedLoaderFactory.kt
@@ -0,0 +1,19 @@
+package com.ruangaldo.mycrypto.factories
+
+import com.ruangaldo.mycrypto.domain.RemoteCryptoFeedLoader
+import com.ruangaldo.mycrypto.http.usecases.CryptoFeedLoader
+
+/**
+ * Written with joy and smile by Ruang Aldo on 04/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+class RemoteCryptoFeedLoaderFactory {
+    companion object {
+        fun createRemoteCryptoFeedLoader(): CryptoFeedLoader {
+            return RemoteCryptoFeedLoader(
+                CryptoFeedHttpClientFactory.createCryptoFeedHttpClient()
+            )
+        }
+    }
+}
