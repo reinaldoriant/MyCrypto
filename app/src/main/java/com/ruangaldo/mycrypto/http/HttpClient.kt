89e9dfe42652cd3d5867f211569ede5f76bd5c90
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 16:51:30 +0700

    + add success response handler

--- app/src/main/java/com/ruangaldo/mycrypto/http/HttpClient.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/http/HttpClient.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.Flow
  */
 
 sealed class HttpClientResult {
+    data class Success(val data: List<RemoteRootCryptoFeed>) : HttpClientResult()
     data class Failure(val throwable: Throwable) : HttpClientResult()
 }
 
