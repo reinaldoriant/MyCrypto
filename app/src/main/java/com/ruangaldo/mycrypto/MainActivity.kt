9d24055f335f1dd63089fcb3e9e82c18ef5ee61f
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 23:49:39 +0700

    ! modify ui and add view model to load data

--- app/src/main/java/com/ruangaldo/mycrypto/MainActivity.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ruangaldo.mycrypto.domain.CryptoFeedViewModel
 import com.ruangaldo.mycrypto.ui.theme.MyCryptoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CryptoFeedRoute()
                 }
             }
         }
@@ -30,17 +32,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CryptoFeedRoute(
+    viewModel: CryptoFeedViewModel = viewModel(factory = CryptoFeedViewModel.FACTORY)
+) {
+    CryptoFeedScreen()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyCryptoTheme {
-        Greeting("Android")
-    }
+fun CryptoFeedScreen() {
+    Text(text = "Starter Project")
 }
