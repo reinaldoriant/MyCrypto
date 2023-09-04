7bfbacced5983e4b7f9b6fd4db1c912657e654b1
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-04 17:04:17 +0700

    + add request response success on 200 HTTP Response

--- app/src/main/java/com/ruangaldo/mycrypto/http/CryptoFeedRetrofitHttpClient.kt
+++ app/src/main/java/com/ruangaldo/mycrypto/http/CryptoFeedRetrofitHttpClient.kt
@@ -8,15 +8,29 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import retrofit2.HttpException
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Written with joy and smile by Ruang Aldo on 04/09/23.
  * Github: https://github.com/reinaldoriant
  */
 
-class CryptoFeedRetrofitHttpClient: HttpClient {
+
+interface CryptoFeedService {
+    @GET("data/top/totaltoptiervolfull")
+    suspend fun get(
+        @Query("limit") limit: Int? = 20,
+        @Query("tsym") tsym: String? = "USD"
+    ): RemoteRootCryptoFeed
+}
+
+class CryptoFeedRetrofitHttpClient constructor(private val cryptoFeedService: CryptoFeedService) :
+    HttpClient {
     override fun get(): Flow<HttpClientResult> = flow {
-        try { } catch (throwable: Throwable) {
+        try {
+            emit(HttpClientResult.Success(cryptoFeedService.get()))
+        } catch (throwable: Throwable) {
             if (throwable is IOException) {
                 emit(HttpClientResult.Failure(Connectivity()))
             }
